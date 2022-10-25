74d01e273b2fb664832286b375d36503e6338531
polina_storozh <58592174+voird@users.noreply.github.com>
2022-10-25 21:43:46 +0300

    Update client.cpp

--- client.cpp
+++ client.cpp
@@ -1,35 +1,27 @@
 #include <iostream>
 #include "fstream"
-
+#include <string>
 
 using namespace std;
 
-struct Students{
-    char name[25];
-    int grades[4];
-    int salary;
-    string client_name;
+struct Student {
+    char name[25]{};
+    int grades[4]{};
+    int answer{};
+    string client_name{};
 };
 
-bool compare(char a[25], char a2[25]){
-    for(int i = 0; i < 25; i++){
-        if (a[i] != a2[i])
-            return false;
-    }
-    return true;
-}
-
-void answering(const string &file, int pos){
-    int c = pos; // позиция в файле
-    int answer; //ответ
-    while(true){
-        ifstream in(file,ios::binary); //ввод в файл
-        if(in){
-            in.seekg(c); //ставит в заданную позицию
-            in.read((char *) &answer, sizeof(answer)); //запись в файл ответа
-            if(c < in.tellg()){ // если начальная позиция меньше позиции в файле
-                cout<<"Answer: "<<answer<<endl; // выводим ответ
-                c += sizeof(answer); // добавляем к нашей позиции новые данные
+void listen(const string& file, int position) {
+    int cursor = position;
+    int answer;
+    while (true) {
+        ifstream in(file, ios::binary);
+        if (in) {
+            in.seekg(cursor);
+            in.read((char*)&answer, sizeof(answer));
+            if (cursor < in.tellg()) {
+                cout << "Answer: " << answer << "\n";
+                cursor += sizeof(answer);
                 break;
             }
             in.close();
@@ -37,31 +29,34 @@ void answering(const string &file, int pos){
     }
 }
 
-int main(){
+int main() {
+    setlocale(LC_ALL, "rus");
     string client_name;
-    cout<<"Name of client: \n";
+    cout << "Введите имя клиента:\n";
     getline(cin, client_name);
-    while(true){
-        Students student;
-        cout<<"Name of student: \n";
-        cin>>student.name;
-        cout<<"Marks of student: \n";
-        for(int i = 0; i<4; i++)
-            cin>>student.grades[i];
+    while (true) {
+        Student student{};
+        cout << "Введите имя:\n";
+        cin >> student.name;
+        cout << "Введите оценки:\n";
+        for (int i = 0; i < 4; i++)
+            cin >> student.grades[i];
         student.client_name = client_name;
-        ofstream out("../second scenery/server.sv", ios::binary | ios::app);
+        ofstream out("../server.sv", ios::binary | ios::app);
         out.write((char*)&student, sizeof(student));
         out.close();
-        ifstream in("../second scenery/"+client_name + ".c", ios::binary);
-        int position;
-        if (in){
-            in.seekg(0,ios::end);
-            position = in.tellg();
+
+        ifstream in("../clients/" + client_name + ".client", ios::binary);
+        int pos;
+        if (in) {
+            in.seekg(0, ios::end);
+            pos = in.tellg();
             in.close();
         }
-        else
-            position = 0;
-        answering("../second scenery/" +client_name + ".c", position);
+        else {
+            pos = 0;
+        }
+        listen("../clients/" + client_name + ".client", pos);
     }
     return 0;
 }
